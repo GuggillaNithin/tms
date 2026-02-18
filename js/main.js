71203edaa21019ca0b9b753a6fc8c36bb750cd0e
Guggillanithin <217r1a6782@cmrtc.ac.in>
2026-02-18 16:16:50 +0530

    Changed index page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import searchFunc from "./search.js"
 
 //! add product to localstorage start
 
+
+
 (async function () {
 
     const response = await fetch("js/data.json")
@@ -47,42 +49,42 @@ import searchFunc from "./search.js"
 
 //! add cartItem to localstorage start
 
-const cartItem = document.querySelector(".header-cart-count")
+// const cartItem = document.querySelector(".header-cart-count")
 
-cartItem.innerHTML = localStorage.getItem("cart")
-    ? JSON.parse(localStorage.getItem("cart")).length
-    : "0"
+// cartItem.innerHTML = localStorage.getItem("cart")
+//     ? JSON.parse(localStorage.getItem("cart")).length
+//     : "0"
 
 //! add cartItem to localstorage end
 
 
 //! modal dialog start
 
-const modal = document.querySelector(".modal-dialog")
-const modalContent = document.querySelector(".modal-dialog .modal-content")
-const btnModalClose = document.querySelector(".modal-dialog .modal-close")
+const modal = document.querySelector(".modal-dialog");
+const modalContent = document.querySelector(".modal-content");
+const btnModalClose = document.querySelector(".modal-close");
 
-
-if (btnModalClose) {
-    btnModalClose.addEventListener("click", () => {
-        modal.classList.remove("show")
-    })
-}
-
-
-if (modal) {
-    document.addEventListener("click", (e) => {
-        if (!e.composedPath().includes(modalContent)) {
-            modal.classList.remove("show")
-        }
-    })
-}
-
-
+// Show after 3 seconds
 if (modal) {
     setTimeout(() => {
-        modal.classList.add("show")
-    }, 3000)
+        modal.classList.add("show");
+    }, 3000);
+}
+
+// Close button
+if (btnModalClose) {
+    btnModalClose.addEventListener("click", () => {
+        modal.classList.remove("show");
+    });
+}
+
+// Close when clicking outside content
+if (modal) {
+    modal.addEventListener("click", (e) => {
+        if (!modalContent.contains(e.target)) {
+            modal.classList.remove("show");
+        }
+    });
 }
 
 

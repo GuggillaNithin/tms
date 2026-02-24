5a6ded97fc701012c2871d5cc8d9ccee74c8234c
Guggillanithin <217r1a6782@cmrtc.ac.in>
2026-02-24 22:26:18 +0530

    added more products

--- js/header.js
+++ js/header.js
@@ -187,6 +187,15 @@ function headerFunc() {
 }
 
 
+const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
 
+  function showNextSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }
+
+  setInterval(showNextSlide, 3000);
 
 export default headerFunc()
